4922105a713e7cb50ac089e381ea659feb65e351
nareshsirvi95 <149091619+nareshsirvi95@users.noreply.github.com>
2023-10-26 21:48:14 +0530

    minor

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using DotNetCoreSqlDb.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DotNetCoreSqlDb.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
